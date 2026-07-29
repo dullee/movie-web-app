9e7396a7fc1fbdaefe59139aebbc89eea0808f97
dullee <91831552+dullee@users.noreply.github.com>
2026-07-29 10:14:53 +0800

    feat: remove redundant dark background from PageMoviesSkeleton; streamline styling for improved clarity

--- app/_components/pageMoviesSkeleton.tsx
+++ app/_components/pageMoviesSkeleton.tsx
@@ -6,8 +6,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function PageMoviesSkeleton() {
   return (
     <div>
-      <div className="mb-8 dark:bg-black">
-        <Skeleton className="dark:bg-black w-37 h-10"></Skeleton>
+      <div className="mb-8 ">
+        <Skeleton className=" w-37 h-10"></Skeleton>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-5 gap-5 dark:bg-black md:gap-8">
         {Array.from({ length: 20 }).map((_, i) => (
